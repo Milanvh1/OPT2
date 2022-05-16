d2c98b7943e5f66015da9d55d98161cac32740f9
Milan van Heijningen <101335237+Milanvh1@users.noreply.github.com>
2022-05-16 22:29:37 +0200

    Delete Test.java

--- Test.java
+++
@@ -1,6 +0,0 @@
-package com.company;
-
-public class Test {
-// Het implementeren van de tests werkt niet voor één of andere manier.
-// Ik had in PROJ1 alle tests geschreven, dus ik hoop dat dit overkeken kan worden.
-}
